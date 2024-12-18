0e96e20d5a1d878a987ba735f107eabe047ba21c
zc <zcgeffen@gmail.com>
2024-12-18 19:18:05 +0200

    add provider file with s3

--- Terraforme/provider.tf
+++ Terraforme/provider.tf
@@ -1,19 +1,15 @@
+
 provider "aws" {
-  region = "us-east-1" # Change to your desired AWS region
+  region                  = "us-east-1" # AWS region
+  shared_credentials_file = "~/.aws/credentials" # Path to AWS credentials file
 }
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket" # Replace with your S3 bucket name
-    key            = "terraform/state.tfstate"   # Replace with your desired state file path
+    bucket         = "terraform-state-bucket" # S3 bucket name
+    key            = "terraform/state.tfstate"   # desired state file path
     region         = "us-east-1"                 # AWS region for the bucket
     encrypt        = true                          # Encrypt the state file
-    dynamodb_table = "terraform-lock"            # Optional, for state locking (use DynamoDB)
+    dynamodb_table = "terraform-lock"            # Optional, for state locking (use DynamoDB) 
   }
-}
-
-# Example resource to test the configuration
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket-name"
-  acl    = "private"
 }
