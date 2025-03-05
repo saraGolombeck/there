f21d87e02688f8344e71f7c8f886580baeef95c7
saraGolombeck <sara.beck.dev@gmail.com>
2025-03-05 21:35:44 +0200

    tmp - 3

--- k8s/setup-cluster.sh
+++ k8s/setup-cluster.sh
@@ -1,28 +1,3 @@
-# #!/bin/bash
-# # Script to set up a Kubernetes cluster with two servers - manager (server1-manager) and worker (server2)
-
-# # Delete existing cluster if any
-# minikube delete
-
-# # Start a new cluster with two nodes
-# echo "Creating a minikube cluster with two nodes..."
-# minikube start --nodes=2 -p my-cluster
-
-# # Assign names to the servers as shown in the example
-# echo "Setting name for the first server: server1-manager..."
-# kubectl label nodes my-cluster kubernetes.io/hostname=server1-manager
-
-# echo "Setting name for the second server: server2..."
-# kubectl label nodes my-cluster-m02 kubernetes.io/hostname=server2
-
-# # Update and display node status
-# echo "Displaying nodes and their labels:"
-# kubectl get nodes --show-labels
-
-# echo "The cluster is ready with two servers:"
-# echo "1. server1-manager - for the database"
-# echo "2. server2 - for the backend and frontend"
-# echo "You can now run: kubectl apply -f ."
 #!/bin/bash
 # Script to set up a Kubernetes cluster with K3D
 
